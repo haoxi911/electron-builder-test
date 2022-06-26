2d227e3f60336bf345b9f9cf5eea455d497358dc
Hao Xi <haoxi911@gmail.com>
2022-06-26 17:51:31 -0400

    Test app

---
+++ .electron-builder.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  appId: 'com.example.myapp',
+  productName: "Picture Keeper",
+  win: {
+    executableName: 'MyApp',
+  },
+  mac: {
+    executableName: 'MyAppForMac',
+    category: 'public.app-category.lifestyle',
+  }
+};
+
+module.exports = config;
